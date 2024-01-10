f3725228d94bd36c9a8627fbfdd8e01a35e754fa
Arian <Arianbod@outlook.com>
2024-01-11 02:57:40 +0300

    Page state added

--- src/components/Context.jsx
+++ src/components/Context.jsx
@@ -2,12 +2,15 @@ import React, { useContext, createContext, useState } from 'react';
 const AppContext = createContext();
 export const AppProvider = ({ children }) => {
 	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+	const [pageId, setPageId] = useState(null);
 	const openSidebar = () => setIsSidebarOpen(true);
 	const closeSidebar = () => setIsSidebarOpen(false);
 	const Values = {
 		isSidebarOpen,
 		openSidebar,
 		closeSidebar,
+		pageId,
+		setPageId,
 	};
 	return <AppContext.Provider value={Values}>{children}</AppContext.Provider>;
 };
