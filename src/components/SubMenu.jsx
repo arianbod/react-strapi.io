df07ff06fdb0aa7530a440981c7ff9b3471d4509
Arian <Arianbod@outlook.com>
2024-01-11 02:58:14 +0300

    Sub menu built

--- src/components/SubMenu.jsx
+++ src/components/SubMenu.jsx
@@ -1,7 +1,41 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { useGlobal } from './Context';
+import sublinks from '../data';
 const SubMenu = () => {
-	return <div>SubMenu</div>;
+	const { pageId, setPageId } = useGlobal();
+	const [submenuItems, setSubmenuItems] = useState([]);
+	const [foundItemPage, setFoundItemPage] = useState(null);
+	useEffect(() => {
+		const foundItem = sublinks.find((item) => item.pageId === pageId);
+		foundItem
+			? (setSubmenuItems(foundItem?.links), setFoundItemPage(foundItem?.page))
+			: (setSubmenuItems([]), setFoundItemPage(null));
+	}, [pageId]);
+	return (
+		<section
+			className={pageId ? 'sub-menu show-submenu' : 'sub-menu'}
+			onMouseLeave={() => setPageId(null)}>
+			<h5>{foundItemPage}</h5>
+			<section
+				className='sub-menu-container'
+				style={{
+					gridTemplateColumns: submenuItems?.length > 3 ? '1fr 1fr' : '1fr',
+				}}>
+				{submenuItems?.map((sublink) => {
+					const { icon, url, label, id } = sublink;
+					console.log(sublink);
+					return (
+						<a
+							href={url}
+							key={id}>
+							{icon}
+							{label}
+						</a>
+					);
+				})}
+			</section>
+		</section>
+	);
 };
 
 export default SubMenu;
