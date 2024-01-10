edca18342cd7eef2c1512663ffbc629bdbbd7964
Arian <Arianbod@outlook.com>
2024-01-10 04:00:33 +0300

    sub components generated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,39 +1,18 @@
 import React from 'react';
 import { useGlobal } from './Context';
 import { FaBars, FaTimes } from 'react-icons/fa';
-import sublinks from '../data';
+import LinksSIdebar from './LinksSIdebar';
 const Sidebar = () => {
 	const { isSidebarOpen, closeSidebar } = useGlobal();
 	return (
 		<aside className={isSidebarOpen ? 'sidebar show' : 'sidebar'}>
 			<div className='sidebar-container'>
-				<button className='close-btn'>
+				<button
+					className='close-btn'
+					onClick={closeSidebar}>
 					<FaTimes />
 				</button>
-				<div className='sidebar-links'>
-					{sublinks.map((item) => {
-						const { links, page, pageId } = item;
-						console.log(item);
-						return (
-							<article key={pageId}>
-								<h1>{page}</h1>
-								<div className='sidebar-sublinks'>
-									{links.map((link) => {
-										const { url, icon, label, id } = link;
-										return (
-											<a
-												key={id}
-												href={url}>
-												{icon}
-												{label}
-											</a>
-										);
-									})}
-								</div>
-							</article>
-						);
-					})}
-				</div>
+				<LinksSIdebar />
 			</div>
 		</aside>
 	);
