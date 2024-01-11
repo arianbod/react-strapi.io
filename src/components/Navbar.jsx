e1cea231511f0797298635c9558d12bb489649f5
Arian <Arianbod@outlook.com>
2024-01-11 03:37:41 +0300

    Mouse handler added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SubMenu from './SubMenu';
 import { FaBars } from 'react-icons/fa';
 import { useGlobal } from './Context';
 import NavLinks from './NavLinks';
+
 const Navbar = () => {
-	const { isSidebarOpen, openSidebar } = useGlobal();
+	const {
+		isSidebarOpen,
+		openSidebar,
+		setPageId,
+		mouseInSubmenu,
+		setMouseInSubmenu,
+	} = useGlobal();
+
+	const [leaveTimer, setLeaveTimer] = useState(null);
+
+	useEffect(() => {
+		// Clear the timeout when the component unmounts
+		return () => clearTimeout(leaveTimer);
+	}, [leaveTimer]);
+
+	const handleMouseLeave = () => {
+		const timer = setTimeout(() => {
+			if (!mouseInSubmenu) {
+				setPageId(null);
+				setMouseInSubmenu(false);
+			}
+		}, 300); // Delay of 300ms
+		setLeaveTimer(timer);
+	};
+
+	const handleMouseEnter = () => {
+		clearTimeout(leaveTimer); // Clear timeout when mouse enters
+	};
+
 	return (
-		<nav className='nav-center'>
+		<nav
+			className='nav-center'
+			onMouseEnter={handleMouseEnter}
+			onMouseLeave={handleMouseLeave}>
 			<h3 className='logo'>Strapi</h3>
 			<NavLinks />
 			<SubMenu />
